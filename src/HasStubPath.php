650301b1713aae4ba4ab7d6a7c780403758105ee
Naoray <krishan.koenig@gmail.com>
2020-04-19 15:08:25 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/HasStubPath.php
+++ src/HasStubPath.php
@@ -2,14 +2,15 @@
 
 namespace Naoray\BlueprintNovaAddon;
 
-trait HasStubPath {
+trait HasStubPath
+{
     /**
      * Returns the stub path for this package.
-     * 
+     *
      * @return string
      */
     protected function stubPath(): string
     {
-        return dirname(__DIR__) . '/stubs';
+        return dirname(__DIR__).'/stubs';
     }
 }
